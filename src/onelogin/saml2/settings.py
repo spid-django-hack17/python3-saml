0282778e5d20231a9c53f16a95719cf34b7c95b3
guglielmo <guglielmo.celata@gmail.com>
2017-10-09 11:43:25 +0200

    regexp used in url validation adapted to the very special case of spid testenv, which has a non-standard domain

--- src/onelogin/saml2/settings.py
+++ src/onelogin/saml2/settings.py
@@ -35,6 +35,7 @@ url_regex = re.compile(
     r'^(?:[a-z0-9\.\-]*)://'  # scheme is validated separately
     r'(?:(?:[A-Z0-9_](?:[A-Z0-9-_]{0,61}[A-Z0-9_])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|'  # domain...
     r'localhost|'  # localhost...
+    r'spid-testenv-identityserver|'
     r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}|'  # ...or ipv4
     r'\[?[A-F0-9]*:[A-F0-9:]+\]?)'  # ...or ipv6
     r'(?::\d+)?'  # optional port
